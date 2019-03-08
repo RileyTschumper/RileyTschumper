62fda99d91c53b85a9682cc67351b95a0277f35e
Riley Tschumper <riley.tschumper@gmail.com>
2019-03-08 15:57:20 -0600

    added json for nav bar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,79 @@
 function InitIndex() {
-  getNewsInfo();
+  //getNavBar();
+  getData(populateNav, "json/nav.json");
+  //getNewsInfo();
+  getData(populateNews, "json/news.json");
+}
+
+function InitResume() {
+  getNavBar();
+  getResumeInfo();
+}
+
+var getData = function(populateLocation, url) {
+  var req = new XMLHttpRequest();
+
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      console.log("Resume data recieved");
+      populateLocation(JSON.parse(req.responseText));
+    }
+  };
+
+  req.open("GET", url, true);
+  req.send();
+};
+
+var populateNav = function(data) {
+  var name = document.createElement("a");
+  name.id = "name";
+  name.setAttribute("href", data[0].home_link);
+  name.innerHTML = data[0].name;
+  document.getElementById("top-nav").appendChild(name);
+  var navContainer = document.createElement("div");
+  navContainer.className = "nav-container";
+
+  var home = document.createElement("a");
+  home.setAttribute("href", data[0].home_link);
+  home.innerHTML = data[0].home;
+  navContainer.appendChild(home);
+  var projects = document.createElement("a");
+  projects.setAttribute("href", data[0].projects_link);
+  projects.innerHTML = data[0].projects;
+  navContainer.appendChild(projects);
+  var resume = document.createElement("a");
+  resume.setAttribute("href", data[0].resume_link);
+  resume.innerHTML = data[0].resume;
+  navContainer.appendChild(resume);
+  document.getElementById("top-nav").appendChild(navContainer);
+};
+
+function getNavBar() {
+  var req = new XMLHttpRequest();
+
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      console.log("Resume data recieved");
+      populateNav(JSON.parse(req.responseText));
+    }
+  };
+
+  req.open("GET", "json/nav.json", true);
+  req.send();
+}
+
+function getResumeInfo() {
+  var req = new XMLHttpRequest();
+
+  req.onreadystatechange = function() {
+    if (req.readyState == 4 && req.status == 200) {
+      console.log("Resume data recieved");
+      populateResume(JSON.parse(req.responseText));
+    }
+  };
+
+  req.open("GET", "json/resume.json", true);
+  req.send();
 }
 
 function getNewsInfo() {
@@ -16,7 +90,11 @@ function getNewsInfo() {
   req.send();
 }
 
-function populateNews(data) {
+function populateResume(data) {
+  for (var i = 0; i < data.length; i++) {}
+}
+
+var populateNews = function(data) {
   for (var i = 0; i < data.length; i++) {
     var newsDiv = document.createElement("div");
     newsDiv.className = "news-container";
@@ -41,4 +119,4 @@ function populateNews(data) {
     newsDivRight.appendChild(text);
     document.body.appendChild(newsDiv);
   }
-}
+};
